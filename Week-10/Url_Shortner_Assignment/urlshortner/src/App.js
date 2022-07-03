5085ab8b8c9476aa61755be27858785af3a0ac41
Mayur Walke <MayurWalke13@gmail.com>
2022-07-03 14:47:43 +0530

    week12 assignment

---
+++ Week-10/Url_Shortner_Assignment/urlshortner/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import inputshortner from './inputshortner';
+function App() {
+  return (
+    <div className="container">
+    <inputshortner/>
+    </div>
+  );
+}
+
+export default App;
